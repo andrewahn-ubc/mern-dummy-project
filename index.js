976e5d47a38bfc261423d9bd14ccf3fd9cb5bf84
Andrew Ahn <andrewahn21@gmail.com>
2024-01-09 12:45:46 -0800

    Tried changing dirname in express middleware.

--- index.js
+++ index.js
@@ -34,12 +34,12 @@ app.use((req, res, next) => {
 app.use('/api/workouts', workoutRoutes)
 
 // ... other app.use middleware 
-app.use(express.static(path.join(__dirname, "client", "build")))
+app.use(express.static(path.join('MERN', "client", "build")))
 
 // ...
 // Right before your app.listen(), add this:
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.join('MERN', "client", "build", "index.html"));
 });
 
 // connect to db
